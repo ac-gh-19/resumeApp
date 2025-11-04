e171e5e853e73067b03779a0567215c02ba584ae
ac-gh-19 <chanandrew1925@gmail.com>
2025-11-04 14:16:29 -0800

    feat: add divider component
    inserts a bar that spans it's parents container to divide component above and below it

---
+++ src/components/Divider.jsx
@@ -0,0 +1,5 @@
+export default function Divider() {
+    return (
+    <div className="border border-stone-700 mt-3 mb-3"></div>
+    )
+}
---
+++ src/components/SectionHeader.jsx
@@ -0,0 +1,22 @@
+import Divider from "./Divider";
+
+export default function SectionHeader({ title, button = false, onClick }) {
+  return (
+    <>
+      <div className="flex justify-between">
+        <h1 className="text-lg font-semibold">{title}</h1>
+        {button ? (
+          <button
+            className="border rounded-md px-3 border-stone-400 bg-stone-700"
+            onClick={() => onClick()}
+          >
+            Add {title}
+          </button>
+        ) : (
+          ""
+        )}
+      </div>
+      <Divider></Divider>
+    </>
+  );
+}
